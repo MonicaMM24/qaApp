d55a5692ce2166ad27a764607139553f74c338f0
MonicaMM24 <monica.mager123@gmail.com>
2024-05-26 00:41:00 +0300

    Additions to the frontend

--- qa-app-frontend/src/Components/Certifications/Certifications.js
+++ qa-app-frontend/src/Components/Certifications/Certifications.js
@@ -1,10 +1,27 @@
 import React from "react";
+import './Certifications.css'; // Import the CSS file
+import photo1 from './photo1.png'; // Import the images
+import photo2 from './photo2.png';
+import photo3 from './photo3.png';
 
 const Certifications = () => {
     return (
-        <div>
+        <div className="certifications-container">
             <h2>Certifications</h2>
-            {/*Certifications elements here*/}
+            <div className="certifications-grid">
+                <div className="certification-item">
+                    <img src={photo1} alt="Certification 1" />
+                    <p>Certification Name 1</p>
+                </div>
+                <div className="certification-item">
+                    <img src={photo2} alt="Certification 2" />
+                    <p>Certification Name 2</p>
+                </div>
+                <div className="certification-item">
+                    <img src="https://istqb-main-web-prod.s3.amazonaws.com/media/original_images/CTEL-TM-STM.png" alt="Certification 3" />
+                    <p>Certification Name 3</p>
+                </div>
+            </div>
         </div>
     );
 };
--- qa-app-frontend/src/Components/Quiz/Quiz.js
+++ qa-app-frontend/src/Components/Quiz/Quiz.js
@@ -1,126 +1,150 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import './Quiz.css'; //Styling for the component
 
 const Quiz = () => {
-    //Quiz questions data
+    // Quiz questions data
     const [questions, setQuestions] = useState([
         {
-            id:1,
-            text: 'An input field takes the year of birth between 1900 and 2004.The boundary values for testing this field are:',
+            id: 1,
+            text: '1. An input field takes the year of birth between 1900 and 2004.The boundary values for testing this field are:',
             options: ['0,1900, 2004, 2005', '1900, 2004', '1899, 1900,1901,2003,2004,2005', '1899,1900,2004,2000'],
             answer: '1899, 1900,1901,2003,2004,2005'
         },
         {
             id:2,
-            text: 'Which one of the following is a non-functional testing method?',
+            text: '2. Which one of the following is a non-functional testing method?',
             options:['System testing', 'Usability testing', 'Performance testing', 'Both B & C'],
             answer: 'Both B & C'
         },
         {
             id:3,
-            text: 'Which of the following tools would be involved in the automation of regression tests?',
+            text: '3. Which of the following tools would be involved in the automation of regression tests?',
             options:['Data tester', 'Boundary tester', 'Capture/Playback', 'Output comparator'],
             answer: 'Capture/Playback'
         },
         {
             id:4,
-            text: 'The incorrect form of logic coverage is:',
+            text: '4. The incorrect form of logic coverage is:',
             options:['Statement Coverage', 'Pole Coverage', 'Condition Coverage', 'Path Coverage'],
             answer: 'Pole Coverage'
         },
         {
             id:5,
-            text: 'Which of the following is not a quality characteristic listed in ISO 9126 Standard?',
+            text: '1. Which of the following is not a quality characteristic listed in ISO 9126 Standard?',
             options:['Functionality', 'Usability', 'Supportability', 'Maintainability'],
             answer: 'Supportability'
         },
         {
             id:6,
-            text: 'To test a function, the programmer has to write a _________, which calls the process to be tested and passes its test data.',
+            text: '2. To test a function, the programmer has to write a _________, which calls the process to be tested and passes its test data.',
             options:['Stub', 'Driver', 'Proxy', 'None of the above'],
             answer: 'Driver'
         },
         {
             id:7,
-            text: 'Boundary value testing:',
+            text: '3. Boundary value testing:',
             options:['It is the same as equivalence partitioning tests', 'Test boundary conditions on, below and above the edges of input and output equivalence classes', 'Tests combinations of input circumstances', 'It is used in the white box testing strategy'],
             answer: 'Test boundary conditions on, below and above the edges of input and output equivalence classes'
         },
         {
             id:8,
-            text: 'Pick the best definition of quality:',
+            text: '4. Pick the best definition of quality:',
             options:['Quality is job one', 'Zero defects', 'Conformance to requirements', 'Work as designed'],
             answer: 'Conformance to requirements'
         },
         {
             id:9,
-            text: 'Fault Masking is:',
+            text: '1. Fault Masking is:',
             options:['Error condition hiding another error condition', 'Creating a test case that does not reveal a fault', 'Masking a fault by a developer', 'Masking a fault by a tester'],
             answer: 'Error condition hiding another error condition'
         },
         {
             id:10,
-            text: 'One Key reason why developers have difficulty testing their work is:',
+            text: '2. One Key reason why developers have difficulty testing their work is:',
             options:['Lack of technical documentation', 'Lack of test tools on the market for developers', 'Lack of training', 'Lack of Objectivity'],
             answer: 'Lack of Objectivity'
         },
         {
             id:11,
-            text: 'Acceptance test cases are based on what?',
+            text: '3. Acceptance test cases are based on what?',
             options:['Requirements', 'Design', 'Code', 'Decision table'],
             answer: 'Requirements'
         },
         {
             id:12,
-            text: 'Code Coverage is used as a measure of what?',
+            text: '4. Code Coverage is used as a measure of what?',
             options:['Defects', 'Trends analysis', 'Test Effectiveness', 'Time Spent Testing'],
             answer: 'Test Effectiveness'
         }
+
     ]);
 
-    //Handle options change
+    const [userAnswers, setUserAnswers] = useState({});
+    const [submittedQuizzes, setSubmittedQuizzes] = useState([]);
 
+    // Handle options change
     const handleOptionChange = (questionId, option) => {
-        setQuestions(questions.map(q =>
-            q.id === questionId ? {...q, answer: option} : q
-        ));
+        setUserAnswers({
+            ...userAnswers,
+            [questionId]: option
+        });
     };
 
-    //Handle form submission
-    const handleSubmit = (event) => {
+    // Handle form submission
+    const handleSubmit = (event, quizIndex) => {
         event.preventDefault();
+        setSubmittedQuizzes([...submittedQuizzes, quizIndex]);
         let questionsAnswers = questions.map(question => ({
             questionId: question.id,
-            answer: question.answer
+            answer: userAnswers[question.id] || null
         }));
         console.log('Submit answers:', questionsAnswers);
-        //Here you might want to send the responses to the server
+        // Here you might want to send the responses to the server
     };
 
-    return (
-        <form onSubmit={handleSubmit} className="quiz">
-            <h1>Quiz</h1>
-            {questions.map(question => (
-                <div key={question.id} className="question">
-                    <h3>{question.text}</h3>
-                    {question.options.map( option => (
-                        <label key={option}>
-                        <input
-                            type="radio"
-                            name={`question_${question.id}`}
-                            value={option}
-                            checked={question.answer===option}
-                            onChange={() => handleOptionChange(question.id, option)}
-                        />
-                        {option}
-                        </label>
-                    ))}
-                </div>
-            ))}
-            <button type="submit">Submit Answers</button>
-        </form>
-    );
+    // Divide questions into three groups
+    const formGroups = [questions.slice(0, 4), questions.slice(4, 8), questions.slice(8, 12)];
 
+    return (
+        <div className="form-container">
+            {formGroups.map((group, index) => (
+                <form key={index} onSubmit={(e) => handleSubmit(e, index)} className="quiz">
+                    <h1>ISTQB Foundation Mock Test{index + 1}</h1>
+                    {group.map(question => (
+                        <div key={question.id} className="question">
+                            <h3>{question.text}</h3>
+                            {question.options.map(option => {
+                                let className = "option-box";
+                                if (submittedQuizzes.includes(index)) {
+                                    if (option === question.answer) {
+                                        className += " correct";
+                                    } else if (userAnswers[question.id] === option) {
+                                        className += " incorrect";
+                                    }
+                                } else if (userAnswers[question.id] === option) {
+                                    className += " selected";
+                                }
+                                return (
+                                    <div key={option} className={className}>
+                                        <input
+                                            type="radio"
+                                            id={`question_${question.id}_${option}`}
+                                            name={`question_${question.id}`}
+                                            value={option}
+                                            checked={userAnswers[question.id] === option}
+                                            onChange={() => handleOptionChange(question.id, option)}
+                                        />
+                                        <label htmlFor={`question_${question.id}_${option}`}>{option}</label>
+                                    </div>
+                                );
+                            })}
+                        </div>
+                    ))}
+                    <button type="submit" className="submit-button">Submit Answers</button>
+                </form>
+            ))}
+        </div>
+    );
 };
 
 export default Quiz;
